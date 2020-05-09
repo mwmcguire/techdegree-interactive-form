5c320f1a88ccf9372458c1b3fd331f79a62b8201
mwmcguire <mike@mmcguire.io>
2020-05-08 20:41:52 -0400

    job roll drop down finished.  started on t-shirt info

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
-// on page load focus on first text field
+// On page load focus on first text field
 window.addEventListener('load', () => document.getElementById('name').focus());
 
 // hide other job role text input / show when "other" option is selected from job role dropdown
 const otherInput = document.getElementById('other-title');
 const titleSelect = document.getElementById('title');
+otherInput.style.display = 'none';
 
-function showOtherInput() {
-  if (titleSelect.options.selectedIndex === 5) {
+titleSelect.addEventListener('change', () => {
+  if (titleSelect.options[5].selected) {
     otherInput.style.display = 'block';
   } else {
     otherInput.style.display = 'none';
   }
+});
+
+// Filter color options by selected theme
+// When form is loaded, update "Design" and "Color" fields to tell user to select theme before color
+// Hide "Select Theme" option in "Design" menu
+const designSelect = document.getElementById('design');
+designSelect.options[0].style.display = 'none';
+
+// Update "Color" field to read "Please select a T-shirt theme"
+const colorSelect = document.getElementById('color');
+colorSelect.options[0].textContent = 'Please select a T-shirt theme';
+
+// Hide the colors in the "Color" drop down menu
+for (let i = 0; i < colorSelect.options.length; i++) {
+  colorSelect.options[i].style.display = 'none';
 }
 
-showOtherInput();
+// Update "Color" field when corresponding color theme is selected in "Design" drop down
+designSelect.addEventListener('change', (e) => {
+  // if "js puns" is selected
+  if (e.target.value === 'js puns') {
+    console.log('js puns');
+    // hide the "heart js" option elements
+
+    // show the "js puns" element
+    // update the "color" field to first available color
+  }
+
+  // if "heart js" is selected
+  if (e.target.value === 'heart js') {
+    console.log('heart js');
+    // hide th "js puns" option elements
+    // show the "heart js" option elements
+    // update the "color" field to first available color
+  }
+});
